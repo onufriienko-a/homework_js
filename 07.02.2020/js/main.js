8a5eea81d8fc4c0c1a7ed4a5b3816941e8fe28df
123 <str8edge4life13@gmail.com>
2020-02-10 09:41:04 +0200

    some tasks from hw3 + hw4

---
+++ 07.02.2020/js/main.js
@@ -0,0 +1,103 @@
+//task 1 (not sure if it's correct, since newKeyValue returns Array instead of inserted value)
+//Создайте объект person, описывающий персону, с произвольным количеством произвольных полей.
+//С помощью оператора in или typeof проверьте наличие в объекте свойства, прочитанного из prompt,
+//и выведите его на экран. Если свойства нет, то добавляйте его в объект со значением,
+//которое также запрашивается из prompt.
+
+var person = {
+  firstname: 'John',
+	lastname: 'Doe',
+	age: 30,
+  position: 'CEO',
+  salary: 4000,
+  height: 180,
+  weight: 80,
+}
+
+let personKey = prompt('insert key name');
+
+
+if (typeof person[personKey] !== "undefined") {
+  alert(personKey + ' key exists!');
+} else {
+  let newKeyValue = prompt('insert value');
+  person[personKey] = [(newKeyValue)];
+}
+console.log(person);
+
+
+// //task 2
+// //Сгенерируйте объект, описывающий модель телефона, заполнив все свойства значениями,
+// //прочитанными из prompt (например: brand, model, resolution, color...), не используя
+// //вспомогательные переменные. Добавьте этот гаджет персоне, созданной ранее.
+
+var phone = {
+  brand: prompt('insert brand name'),
+  color: prompt('insert color'),
+  model: prompt('insert model'),
+  resolution: prompt('insert resolution'),
+}
+
+person.phone = phone;
+
+console.log(person);
+
+
+// //task 3
+// //Запустите бесконечный цикл. В цикле вызывайте prompt, в котором пишется либо delete,
+// //либо update. В зависимости от указанного действия, удаляйте или добавляйте (редактируйте)
+// //свойство в объекте персоны (которая уже имеет телефон). Какое именно свойство удалять
+// //или добавлять - также читается из prompt. Какое значение будет у добавленного
+// //свойства - тоже берется из prompt. При нажатии на Отмена при появлении prompt редактирование
+// //person заканчивается. И обновленные данные распечатываются в консоли.
+
+let i = '';
+
+while (true){
+  let i = prompt('choose the action');
+    if (i == 'update'){
+    let addKeyName = prompt('insert key name to be added');
+    let addKeyValue = prompt('insert ' + addKeyName + ' value');
+    person[addKeyName] = [(addKeyValue)];
+  } else if (i == 'delete'){
+    let deleteKeyName = prompt('insert key name to be deleted');
+    delete person.deleteKeyName;
+  } else if (i === null){
+    break;
+  } else {
+    alert('wrong action');
+    continue;
+  }
+}
+console.log(person);
+
+
+//task 4 (dd returns undefined)
+//Динамически (скриптом) создайте HTML элемент <dl>. Переберите в цикле (for..in) сгенерированный
+//ранее объект person, добавляя dt - для имени свойства и dd - для значения свойства
+//(работа с DOM - с использованием методов Native JS).
+
+let list = document.createElement('dl');
+document.body.append(list);
+
+for (var key in person) {
+
+  console.log(key, person[key]);
+	let dt = document.createElement('dt');
+	dt.innerText = key;
+
+	let dd = document.createElement('dd');
+	dd.innerText = parent[key];
+
+	list.appendChild(dt);
+	list.appendChild(dd);
+}
+
+// //task 5
+// //Создайте объект dates для хранения дат. Первая дата – позавчера. Вторая дата – текущая дата
+// //(new Date) минус 365 дней. Из prompt читается дата в формате yyyy-MM-dd. Проверьте,
+// //попадает ли введенная дата в диапазон дат объекта dates.
+
+// let dates = {
+//   date1: new Date()
+// }
